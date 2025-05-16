fb2bdb060771d535de6dd61521cc61b7939f8df2
Artur_Assis <assis.artur024@gmail.com>
2025-05-16 18:37:07 -0300

    alterações na pagina de tornieos

--- js/torneios_JS/copa_ralth_2025.js
+++ js/torneios_JS/copa_ralth_2025.js
@@ -1,18 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
   const botoes = document.querySelectorAll('.opc_inf');
 
+  const informacoes = document.getElementById('inf')
+  informacoes.classList.add('mostrar')
+
   botoes.forEach(botao => {
     botao.addEventListener('click', () => {
       const conteudo_aberto = botao.nextElementSibling;
       const abrir = conteudo_aberto.classList.contains('mostrar');
-
-      document.querySelectorAll('.contents').forEach(conteudo => {
-        conteudo.classList.remove('mostrar');
-      });
+      
 
       if (!abrir) {
         conteudo_aberto.classList.add('mostrar');
+      }else{
+        conteudo_aberto.classList.remove('mostrar')
       }
+
+
+
     });
   });
 });
